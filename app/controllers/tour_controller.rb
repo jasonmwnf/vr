6e78714c40921722e779eadbbdd650e47e470427
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-10-15 13:59:04 -0500

    added new seed for events, removed strftime in tour index

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -3,7 +3,7 @@ class TourController < ApplicationController
   def index
     @videos = Video.all.order(id: 'DESC').limit(3)
     @events = Event.all.order(date: 'ASC').limit(3)
-    @future = Video.all
+    @future = UpcomingVideo.all
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/1.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/2.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/3.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/4.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/5.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/6.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/7.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/8.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/9.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/10.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/11.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/12.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/13.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/14.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/15.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/16.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/17.jpg")
+UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/18.jpg")
+
+
+Event.create(name_of_location: "EXXXOTICA New Jersey", type_of_event: "Sex Expo", date: "November 13, 2015", time: "5:00pm", address: "NEW JERSEY CONVENTION AND EXPOSITION CENTER 97 Sunfield Ave. Edison, NJ 08837", phone: "")
+Event.create(name_of_location: "PT's Showclub Miami", type_of_event: "Hosting", date: "September 19, 2015", time: "", address: "7565 W 20th Ave, Hialeah, FL 33014", phone: "(786)-953-7374")
+Event.create(name_of_location: "Sapphires NYC", type_of_event: "Feature Dancing", date: "December 04, 2015", time: "", address: "333 E 60th St, New York, NY 10022", phone: "(212)-421-3600")
