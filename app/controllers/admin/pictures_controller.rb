ea317db02d21d6aaadb08fad4a16649c4d973047
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-10-16 15:50:58 -0500

    added admin models pictures posts users videos

---
+++ app/controllers/admin/pictures_controller.rb
@@ -0,0 +1,28 @@
+class Admin::PicturesController < AdminsController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+end
---
+++ app/controllers/admin/videos_controller.rb
@@ -0,0 +1,27 @@
+class Admin::VideosController < AdminsController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+end
