707b215387736dd7000f847ae49383ac0be92181
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-10-21 01:19:51 -0500

    removed carrier_wave credentials

--- config/initializers/carrier_wave.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env.production?
-  Carrierwave.configure do |config|
-    config.fog_credentials = {
-      :provider => "AWS",
-      :aws_access_key_id => ENV['S3_ACCESS_KEY']
-    }
-  end
-end
