80059f002181c7aa9830a196bcd839ee36e75845
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-10-21 20:44:37 -0500

    added video show and video index

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,16 +1,22 @@
 class VideosController < ApplicationController
   def index
-    @videos = Video.all
+    @latest_vid = Video.last
+    @models     = @latest_vid.models
+    @more_vids  = Video.where.not(id: @latest_vid).order(id: 'DESC')
   end
 
   def show
     @video = find_video
+    @videopics = @video.pictures.paginate(page: params[:page], per_page: 12).order(id: 'ASC')
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
     def find_video
       @video = Video.find(params[:id])
-      @models = @video.models
     end
 
 end
