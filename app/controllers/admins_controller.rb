9c306e3d776c761bf34f6a98442503a28d84a663
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-10-28 02:57:12 -0500

    added authorization to admins controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,10 +2,6 @@ class AdminsController < ApplicationController
   before_filter :authenticate_admin!
 
   def authenticate_admin!
-    if current_user.present? && current_user.is_admin?
-      redirect_to admin_path
-    else
-      redirect_to new_user_session_path
-    end
+    redirect_to new_user_session_path unless current_user.present? && current_user.is_admin?
   end
 end
