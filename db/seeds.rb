eb25e01fa0649504fefb7816aa92b8dc9befee1e
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-11-11 12:17:05 -0600

    added new seeds, and slice(3)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,46 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/1.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/2.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/3.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/4.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/5.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/6.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/7.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/8.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/9.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/10.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/11.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/12.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/13.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/14.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/15.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/16.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/17.jpg")
-UpcomingVideo.create(image: "https://s3-us-west-2.amazonaws.com/vrod-web-assets/upcoming-videos/18.jpg")
 
-
-Event.create(name_of_location: "EXXXOTICA New Jersey", type_of_event: "Sex Expo", date: "November 13, 2015", time: "5:00pm", address: "NEW JERSEY CONVENTION AND EXPOSITION CENTER 97 Sunfield Ave. Edison, NJ 08837", phone: "")
-Event.create(name_of_location: "PT's Showclub Miami", type_of_event: "Hosting", date: "September 19, 2015", time: "", address: "7565 W 20th Ave, Hialeah, FL 33014", phone: "(786)-953-7374")
-Event.create(name_of_location: "Sapphires NYC", type_of_event: "Feature Dancing", date: "December 04, 2015", time: "", address: "333 E 60th St, New York, NY 10022", phone: "(212)-421-3600")
-
-Video.create(
-    title: "Diamonds Make Me Squirt",
-    featured_image: "https://s3-us-west-2.amazonaws.com/veronicarodriguez/033+Diamonds+Make+Me+Squirt/Main+Pictures/5W4A8599.JPG",
-    description: "<p>It’s seems appropriate that before I had a chance to eat my first meal on my new marble table, I got to taste every tantalizing inch of Skin Diamond. She caressed me, teased me, stared into my eyes as she gripped my neck. Cold marble and warm bodies, hot friction and wet tongues, finger licking and hip thrusting, this a meal fit for a queen. Or two.</p>",
-    video_url: "https://s3-us-west-2.amazonaws.com/veronicarodriguez/033+Diamonds+Make+Me+Squirt/33+Diamonds+Make+Me+Squirt.mp4",
-    teaser_video_url: "https://s3-us-west-2.amazonaws.com/veronicarodriguez/033+Diamonds+Make+Me+Squirt/33+Diamonds+Make+Me+Squirt+Trailer.mp4",
-    post_date: "",
-    thumbnail_1: "https://s3-us-west-2.amazonaws.com/veronicarodriguez/033+Diamonds+Make+Me+Squirt/Main+Pictures/5W4A8570.JPG",
-    thumbnail_2: "https://s3-us-west-2.amazonaws.com/veronicarodriguez/033+Diamonds+Make+Me+Squirt/Main+Pictures/5W4A8578.JPG",
-    thumbnail_3: "https://s3-us-west-2.amazonaws.com/veronicarodriguez/033+Diamonds+Make+Me+Squirt/Main+Pictures/5W4A8589.JPG",
-    thumbnail_4: "https://s3-us-west-2.amazonaws.com/veronicarodriguez/033+Diamonds+Make+Me+Squirt/Main+Pictures/5W4A8637.JPG"
-  )
-
-Model.create(
+20.times do
+    Model.create(
         name: "Ashley Cross",
-        portfolio_image: "",
+        portfolio_image: "/uploads/models/Addison%20Avery/portfolio_image/Addison_Avery_Proofile.jpg",
         favorite_position: "",
         birthdate: "",
         astrology: "",
@@ -58,5 +23,4 @@ Model.create(
         tattoos: "",
         pierciings: ""
     )
-
-User.create(email: "pares101@aol.com", password: "putamerda1", is_admin: true)
+end
