d58425def4e69c55b744b9d35ebfdd992b196e09
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-11-15 02:42:40 -0600

    edited registration with form_tag

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
+  def create
+    super
+    binding.pry
+  end
+
+  def method_name
+
+  end
+
+
   protected
 
-  def after_sign_up_path_for(resource)
-    "https://test.wnu.com/secure/services/?api=join&pi_code=cjuuci1p882368&reseller=a"
-  end
+  # def after_sign_up_path_for(resource)
+  #   redirect_to "https://test.wnu.com/secure/services/?api=join&pi_code=cjuuci1p882368&reseller=a" and return
+  # end
 
 end
