811e7c91d004f2bc178442e5aa7a746550277939
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-11-20 13:17:48 -0600

    added DESC to date

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -2,9 +2,9 @@ class TourController < ApplicationController
 
   def index
     @videos = Video.friendly.all.order(id: 'DESC').limit(3)
-    @events = Event.all.order(date: 'ASC').limit(3)
-    @posts = Post.all.order(date: 'ASC').limit(3)
-    @live_shows = LiveShow.all.order(date: 'ASC').limit(3)
+    @events = Event.all.order(date: 'DESC').limit(3)
+    @posts = Post.all.order(date: 'DESC').limit(3)
+    @live_shows = LiveShow.all.order(date: 'DESC').limit(3)
     @future = UpcomingVideo.all
   end
 
