8803818808bcd8be12cc7f6051765df42e535315
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-11-20 13:13:10 -0600

    added live shows to tours index

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -4,6 +4,7 @@ class TourController < ApplicationController
     @videos = Video.friendly.all.order(id: 'DESC').limit(3)
     @events = Event.all.order(date: 'ASC').limit(3)
     @posts = Post.all.order(date: 'ASC').limit(3)
+    @live_shows = LiveShow.all.order(date: 'ASC').limit(3)
     @future = UpcomingVideo.all
   end
 
