198b2e67a11f7df77eb4bec7cb5a1f2da66d2c30
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-11-20 02:32:10 -0600

    added @posts to tour

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -3,6 +3,7 @@ class TourController < ApplicationController
   def index
     @videos = Video.friendly.all.order(id: 'DESC').limit(3)
     @events = Event.all.order(date: 'ASC').limit(3)
+    @posts = Post.all.order(date: 'ASC').limit(3)
     @future = UpcomingVideo.all
   end
 
