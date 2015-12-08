7ff67ba852fd488f2afc867db18d2bb892cb3c46
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-12-08 12:46:43 -0600

    added posts video post show

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   mount_uploader :featured_image, PostFeaturedImageUploader
-
+  mount_uploader :video, VideoBlogPostUploader
   extend FriendlyId
     friendly_id :title, use: [:slugged, :finders]
   validates_presence_of :title
