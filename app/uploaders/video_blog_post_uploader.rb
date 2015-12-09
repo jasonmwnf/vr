a51247b8bc7596faf09149768de531b2e59312ee
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-12-08 21:43:17 -0600

    added video

--- app/uploaders/video_blog_post_uploader.rb
+++ app/uploaders/video_blog_post_uploader.rb
@@ -17,7 +17,7 @@ class VideoBlogPostUploader < CarrierWave::Uploader::Base
 
 
   def store_dir
-    "uploads/posts/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
