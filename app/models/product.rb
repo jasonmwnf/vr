0c2e281bd770e64ac0005ad1e823d82f6f50b0bd
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-12-10 14:35:18 -0600

    added uploader

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ActiveRecord::Base
-
+  mount_uploader :image, ProductImageUploader
 end
--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -17,7 +17,7 @@ class ProductImageUploader < CarrierWave::Uploader::Base
   end
 
   def store_dir
-    "uploads/products/#{model.title.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.title.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
