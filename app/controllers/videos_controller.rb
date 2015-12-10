86385896782a76a0e7a53e9be06b36939e191a21
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-12-09 23:28:49 -0600

    removed aws upload for video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -31,39 +31,5 @@ class VideosController < ApplicationController
     @video = Video.find(params[:id])
   end
 
-  def aws_upload_for video
-    # ...
-    # All AWS File uploading goes here
-    # However, should go in the model
-    # ...
-    puts "\n\n\n"
-    [:original_filename, :content_type].each do |param|
-      puts "#{param.to_s.humanize}: #{video.send param}"
-    end
-    puts "\n\n\n"
-  end
 
-  def upload_response
-    # This is just a dummy response that jquery file upload expects
-
-    name = 'video.mp4'
-    size = 1000
-    url  = "http://example.org/files/#{name}"
-    thumbnail_url = "http://example.org/files/#{name}_thumb.jpg"
-    delete_type = "DELETE"
-    delete_url = "http://example.org/files/picture1.jpg"
-
-    {
-      files: [
-        {
-          name: name,
-          size: size,
-          url: url,
-          thumbnailUrl: thumbnail_url,
-          deleteType: delete_type,
-          deleteUrl: delete_url
-        }
-      ]
-    }
-  end
 end
