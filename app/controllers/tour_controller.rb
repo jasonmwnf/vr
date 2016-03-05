d787c6ef93eff3fe95780070369d74e5e97a9684
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-04 22:17:03 -0600

    added ASC instead of descending

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -4,7 +4,7 @@ class TourController < ApplicationController
     @latest_videos = Video.latest_videos.limit(3)
     @events = Event.all.order(date: 'ASC').limit(3)
     @posts = Post.all.order(date: 'DESC').limit(3)
-    @live_shows = LiveShow.all.order(date: 'DESC').limit(3)
+    @live_shows = LiveShow.all.order(date: 'ASC').limit(3)
     @upcoming_videos = Video.upcoming_videos.limit(3)
     @products = Product.all.limit(3)
   end
