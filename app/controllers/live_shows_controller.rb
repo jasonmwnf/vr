2f334897163f56b47b80b0724928f2fa4c0f0bfc
Gustavo Pares <gustavoanalytics@gmail.com>
2016-03-22 10:18:18 -0500

    order of live show fix

--- app/controllers/live_shows_controller.rb
+++ app/controllers/live_shows_controller.rb
@@ -2,7 +2,7 @@ class LiveShowsController < ApplicationController
 
   def index
     @live_shows = LiveShow.all
-    @recordings = LiveStreamRecording.all
+    @recordings = LiveStreamRecording.all.order(date: 'DESC')
   end
 
 end
