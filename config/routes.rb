240e0820b21435953f465f902b3bb539b47eeb13
Gustavo Pares <gustavoanalytics@gmail.com>
2016-04-02 01:58:23 -0500

    added credits routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations"
   }
   post 'users/status' => 'users#status'
+  post '/credits' => 'live_shows#credits'
   get 'users/outcome' => 'users#outcome'
   get 'users/account' => 'users#account'
   get 'tour/index'
