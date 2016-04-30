cf61a353e024c759886e8e53eabd7b44f8c90d3c
Gustavo Pares <gustavoanalytics@gmail.com>
2016-04-30 12:31:02 -0500

    added new files

--- app/controllers/live_shows_controller.rb
+++ app/controllers/live_shows_controller.rb
@@ -28,7 +28,6 @@ def tip
   if @tip_amount <= @user.credits
     @user.update!(credits: @user.credits - @tip_amount)
     PrivatePub.publish_to("/credits/#{@user.username}", "$('#credits').html('Your credits #{@user.credits}');")
-    PrivatePub.publish_to("/credits/pornpapi}", "alert('#{@user.username} has tipped #{@tip_amount}')")
   else
     PrivatePub.publish_to("/credits/#{@user.username}", "alert('Not enough credits!');")
   end
