9ffca44d2dd3fbb917bdd7aaaab52b4ea08af153
Gustavo Pares <gustavoanalytics@gmail.com>
2016-04-30 15:19:59 -0500

    added new subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
   def private_show(user, amount)
     @user = user
     @amount = amount
-    mail(to: 'pares101@aol.com', subject: 'Test')
+    mail(to: 'pares101@aol.com', subject: 'New private show request')
   end
 end
