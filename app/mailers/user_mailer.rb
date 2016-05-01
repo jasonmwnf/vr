862de1c77966c7537de073251b87560851a8a279
Gustavo Pares <gustavoanalytics@gmail.com>
2016-04-30 20:09:08 -0500

    added correct email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def private_show(user, amount)
     @user = user
     @amount = amount
-    mail(to: 'pares101@aol.com', subject: 'New private show request')
+    mail(to: 'veronica@veronicarodriguez.com', subject: 'New private show request')
   end
 end
